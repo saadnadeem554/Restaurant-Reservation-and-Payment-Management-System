72f6cfe45be53af2c7dba9159c873b38c948efbe
Mustafa Iqbal <112825714+Mustafaiqbal2@users.noreply.github.com>
2024-11-27 18:46:20 +0500

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -3,6 +3,11 @@ module Proj {
 	requires java.sql;
 	requires javafx.graphics;
 	requires javafx.fxml;
+	requires java.security.jgss;
+	requires jdk.security.auth;
+	requires com.microsoft.sqlserver.jdbc;
+	requires com.twilio.sdk;
+
 
 	opens project to javafx.graphics, javafx.fxml, javafx.base;
 	opens UI to javafx.fxml, javafx.base;
