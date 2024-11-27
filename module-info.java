7f5f72f4b58f19b3e40ac3dcc84c20db1a3638f6
Mustafa Iqbal <112825714+Mustafaiqbal2@users.noreply.github.com>
2024-11-27 18:40:28 +0500

    Update module-info.java

--- module-info.java
+++ module-info.java
@@ -3,7 +3,6 @@ module Proj {
 	requires java.sql;
 	requires javafx.graphics;
 	requires javafx.fxml;
-	requires twilio;
 
 	opens project to javafx.graphics, javafx.fxml, javafx.base;
 	opens UI to javafx.fxml, javafx.base;
